642ebd73d04f049f25381c49bb83ad1cd5cc2993
CeciliaOuko <cecilia.ouko@gmail.com>
2022-09-12 12:52:14 -0700

    0-positive_or_negative.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,16 @@
+
 #include <stdio.h>
 
 /**
-*main-print the string in the put function
+*main - print the string in the put function
+*
 *Description: using the main function
 *this program prints "Programming is like building a multilingual puzzle"
-*Return: 0
+*Return:0
 */
 
-int maint(void)
-
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingualpuzzle");
+	return (0);
 }
